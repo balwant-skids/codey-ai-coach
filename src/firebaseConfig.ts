9cc69cad5fa431e2911b39024223507a431af248
satishskid <satish@skids.health>
2025-07-13 11:43:24 +0530

    Update Firebase configuration with real project settings

--- src/firebaseConfig.ts
+++ src/firebaseConfig.ts
@@ -1,8 +1,13 @@
+// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 export const firebaseConfig = {
   apiKey: "AIzaSyCMdhoXzLSZSY7MEavFaBCWOwgqWTZ3vjA",
   authDomain: "codey-1f630.firebaseapp.com",
@@ -15,6 +20,7 @@ export const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 
 // Initialize and export Firebase services
 export const auth = getAuth(app);
